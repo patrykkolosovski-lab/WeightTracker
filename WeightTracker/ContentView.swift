d3aa47bdae80b6db301279a481fabc2e2e0a6912
Patryk <patryk.kolosovski@gmail.com>
2026-03-24 01:45:53 +0100

    Initial Commit

---
+++ WeightTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeightTracker
+//
+//  Created by Patryk Kolosovski on 24/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeightTracker/WeightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightTrackerApp.swift
+//  WeightTracker
+//
+//  Created by Patryk Kolosovski on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
